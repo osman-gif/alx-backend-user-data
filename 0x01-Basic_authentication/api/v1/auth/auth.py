80725fce69e7f3a5b79e17e10bf5f3a09277af07
osman-gif <osmanngong@gmail.com>
2024-11-12 15:03:45 +0200

    Added the auth.py file

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+#!usr/bin/env python3
+"""
+defines a class that is the template for all authentication system
+you will implement.
+"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require_auth
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization_header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user
+        """
+        return None
