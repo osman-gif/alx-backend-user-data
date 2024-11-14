056abe971a29383954849ec958a8b30248285dd3
osman-gif <osmanngong@gmail.com>
2024-11-14 12:16:03 +0200

    Added a new project directory 0x02-Session_authentication

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+defines a class that is the template for all authentication system
+you will implement.
+"""
+
+
+from flask import request
+from typing import List, TypeVar
+import flask
+
+
+class Auth:
+    """ Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ if path is part of the excluded_paths False
+        """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        if not path.endswith('/'):
+            path = path+'/'
+
+        if path in excluded_paths:
+            return False
+        else:
+            return True
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization_header
+        """
+        if request is None:
+            return None
+        if 'Authorization' not in list(request.headers.keys()):
+            return None
+        else:
+            return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user
+        """
+        return None
