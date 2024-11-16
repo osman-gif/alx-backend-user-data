68baf34dd7a1b424a882b3fdd3c44b0890c5c10b
osman-gif <osmanngong@gmail.com>
2024-11-16 10:40:28 +0200

    Fixed indentation problem in app.py file

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,12 +35,13 @@ def before_request():
     if auth.require_auth(request.path, paths) is False:
         pass
     else:
-        if auth.authorization_header(request) is None:
+        auth_header = auth.authorization_header(request)
+        cookie = auth.session_cookie(request)
+        if auth_header is None and cookie is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
-        if auth.session_cookie(request) is None:
-            abort(401)
+
     request.current_user = auth.current_user(request)
 
 
