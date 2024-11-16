7a090f7c21f3221920154261d1f82e98a19858ba
osman-gif <osmanngong@gmail.com>
2024-11-16 11:44:51 +0200

    Added the api/v1/veiws/session_auth.py file and added it to the end of the __init__.py file

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+new view for Session Authentication
+"""
+
+from flask import request, jsonify, abort
+from models.user import User
+from api.v1.views import Blueprint
+
+app_views = Blueprint('app_views', __name__)
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """ POST /auth_session/login
+    """
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email is None or email == "":
+        return jsonify({"error": "email missing"}), 400
+    if password is None or password == "":
+        return jsonify({"error": "password missing"}), 400
+
+    users = User.search({'email': email})
+
+    if users == [] or users is None:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    for user in users:
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+        if user.email == email:
+            from api.v1.app import auth
+            import os
+            session_id = auth.create_session(user.id)
+            response = jsonify(user.to_json())
+            session_name = os.getenv('SESSION_NAME')
+            response.set_cookie(session_name, session_id)
+            return response
