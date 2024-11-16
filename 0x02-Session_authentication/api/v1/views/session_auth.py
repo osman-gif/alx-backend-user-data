94d426d0e3b2321714076c5a11b81c764ec3080a
osman-gif <osmanngong@gmail.com>
2024-11-16 12:14:55 +0200

    Added route decorator to the logout function in the api/v1/veiws/session_auth.py file

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -39,6 +39,10 @@ def login():
             session_name = os.getenv('SESSION_NAME')
             response.set_cookie(session_name, session_id)
             return response
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
 def logout():
     """ DELETE /auth_session/logout
     """
