92e676b256bb3b193bdd8c2b46b3a7c2e53f4da1
osman-gif <osmanngong@gmail.com>
2024-11-20 20:54:12 +0200

    used AUTH instead of Auth in the users method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -28,9 +28,9 @@ def users(email, password):
     """
     try:
         AUTH.register_user(email, password)
-        return jsonify({"email": "{email}", "message": "user created"})
+        return jsonify({"email":"{email}", "message": "user created"})
     except ValueError:
-        return jsonify({"message": "email already registered"}), 400
+        return jsonify({"message":"email already registered"}), 400
 
 
 if __name__ == "__main__":
