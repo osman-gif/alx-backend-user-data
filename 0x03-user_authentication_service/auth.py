2ce5cb975cea2f26eae5653fdf430fb7c32c20d3
osman-gif <osmanngong@gmail.com>
2024-11-23 12:45:46 +0200

    removed a white space from a line

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -46,7 +46,7 @@ class Auth:
         If it matches return True. In any other case, return False
         """
         import bcrypt
-        
+
         try:
             user = self._db.find_user_by(email=email)
             return bcrypt.checkpw(password.encode('utf-8'),
